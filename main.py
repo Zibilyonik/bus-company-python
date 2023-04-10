0bfdbbaf62d76b73e387e9436f389a3c691d35c0
Onur Onat Surmeli <onatsurmeli@gmail.com>
2023-04-10 17:38:54 +0200

    fixed linter errors

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+# python3
 import json
 
-#checks data types of the input
+# checks data types of the input
+
+
 def check_data_type(data):
     if isinstance(data, dict):
         return "dict"
@@ -17,6 +20,7 @@ def check_data_type(data):
     else:
         return "none"
 
+
 def main():
     expected_data_types = {
         "bus_id": "int",
@@ -26,17 +30,21 @@ def main():
         "stop_type": "str",
         "a_time": "str"
     }
-    errors = {"bus_id": 0, "stop_id": 0, "stop_name": 0, "next_stop": 0, "stop_type": 0, "a_time": 0}
+    errors = {"bus_id": 0, "stop_id": 0, "stop_name": 0,
+              "next_stop": 0, "stop_type": 0, "a_time": 0}
     total = 0
     json_data = json.loads(input())
     for item in json_data:
         for key, value in item.items():
-            if check_data_type(value) != expected_data_types[key] or (value == "" and key != "stop_type") or (key == "stop_type" and value not in ["", "S", "O", "F"]):
+            if check_data_type(value) != expected_data_types[key] or \
+                (value == "" and key != "stop_type") or \
+                (key == "stop_type" and value not in ["", "S", "O", "F"]):
                 errors[key] += 1
                 total += 1
     print(f'Type and required field validation: {total} errors')
     for key, value in errors.items():
         print(f"{key}: {value}")
-        
+
+
 if __name__ == "__main__":
     main()
