81ea77c7ef95d9bdde3a285a6a7fa562d20ed42d
Onur Surmeli <onatsurmeli@gmail.com>
2023-04-17 15:41:44 +0200

    added timetable check for correct ordering of times and stops

--- main.py
+++ main.py
@@ -1,87 +1,42 @@
 # python3
 import json
-# import re
 
-# def check_data_type(data):
-#     if isinstance(data, dict):
-#         return "dict"
-#     elif isinstance(data, list):
-#         return "list"
-#     elif isinstance(data, str):
-#         return "str"
-#     elif isinstance(data, int):
-#         return "int"
-#     elif isinstance(data, bool):
-#         return "bool"
-#     elif isinstance(data, float):
-#         return "float"
-#     else:
-#         return "none"
 
-# def check_time(time):
-#     if isinstance(time, str) is False:
-#         return False
-#     if len(time) != 5:
-#         return False
-#     if time[2] != ":":
-#         return False
-#     if not time[:2].isdigit() or not time[3:].isdigit():
-#         return False
-#     if int(time[:2]) > 23 or int(time[3:]) > 59:
-#         return False
-#     return True
-
-# def check_stop_name(name):
-#     regex = r"^([A-Z][a-z]+\s)([A-Z][a-z]+\s)?(Road|Avenue|Boulevard|Street)$"
-#     if re.search(regex, name):
-#         return True
-#     else:
-#         print(name)
-#     return False
+def time_order_check(lines):
+    errors = []
+    for line in range(len(lines)):
+        if line == 0:
+            continue
+        if lines[line]["time"] < lines[line - 1]["time"]:
+            errors.append(line)
+    return errors
 
 def main():
-    # expected_data_types = {
-    #     "bus_id": "int",
-    #     "stop_id": "int",
-    #     "stop_name": "str",
-    #     "next_stop": "int",
-    #     "stop_type": "str",
-    #     "a_time": "str"
-    # }
-    # errors = {"stop_name": 0, "stop_type": 0, "a_time": 0}
     lines = {}
-    types = {"S": [], "F": []}
-    stops = []
-    line_stops = {}
-    transfers = []
-    # total = 0
     json_data = json.loads(input())
     for item in json_data:
-        if item["stop_name"] in stops and item["stop_name"] not in transfers:
-            transfers.append(item["stop_name"])
+        if item["stop_type"] == "S":
+            lines[item["bus_id"]] = [
+                {"bus_id": item["bus_id"], "stop_name": item["stop_name"], "stop_id": item["stop_id"], "time": item["a_time"]}
+            ]
         else:
-            stops.append(item["stop_name"])
-        if item["stop_type"] in ["S", "F"]:
-            if item["bus_id"] not in line_stops:
-                line_stops[item["bus_id"]] = item["stop_type"]
-            else:
-                line_stops[item["bus_id"]] += item["stop_type"]
-            if item["stop_name"] not in types[item["stop_type"]]:
-                types[item["stop_type"]].append(item["stop_name"])
-        if item["bus_id"] not in lines:
-            lines[item["bus_id"]] = [item["stop_name"]]
+            lines[item["bus_id"]].append(
+                {"bus_id": item["bus_id"], "stop_name": item["stop_name"], "stop_id": item["stop_id"], "time": item["a_time"]}
+            )
+    
+    print("Arrival time test:")
+    for line in lines.items():
+        errors = time_order_check(line)
+        if len(errors) == 0:
+            print("OK")
         else:
-            lines[item["bus_id"]].append(item["stop_name"])
-    for line in lines:
-        if line_stops[line] not in  ["SF", "FS"]:
-            print(f"There is no start or end stop for the line: {line}.")
-            return
-    types["S"].sort()
-    transfers.sort()
-    types["F"].sort()
-    print(f'Start stops: {len(types["S"])} {types["S"]}')
-    print(f'Transfer stops: {len(transfers)} {transfers}')
-    print(f'Finish stops: {len(types["F"])} {types["F"]}')
+            checked = []
+            for error in errors:
+                if error["bus_id"] not in checked:
+                    checked.append(error["bus_id"])
+                    print(
+                        f"bus_id line {line}: wrong time on station {lines[line][error]['stop_id']}"
+                    )
 
 
 if __name__ == "__main__":
